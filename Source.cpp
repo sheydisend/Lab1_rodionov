0f921ac1c3c9e4f626e6d3321f727a6532ceb094
sheydisend <107221214+sheydisend@users.noreply.github.com>
2023-12-11 15:31:53 +0300

    MetodGaussa.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,67 +9,6 @@ double* vectorb(double** arra, double* x, int n);
 double* raznost(double* x1, double* x2, int n);
 double norma_vektora(double* F, int n);
 
-int main()
-{
-	setlocale(LC_ALL, "RUS");
-	int n;
-	cout << "vvedite poryadok matrici: ";
-	do
-	{
-		cin >> n;
-	} while (n < 0);
-	double** arra = new double* [n];
-	double* arrb = new double[n];
-	double** copya = new double* [n];
-	double* copyb = new double[n];
-	cout << "\nVvedide matricu: \n";
-	for (int i = 0; i < n; i++)
-	{
-		arra[i] = new double[n];
-		copya[i] = new double[n];
-		for (int j = 0; j < n; j++)
-		{
-			cout << "a[" << i + 1 << "][" << j + 1 << "]: ";
-			cin >> arra[i][j];
-			copya[i][j] = arra[i][j];
-		}
-	}
-	cout << "\nvvedite svobodnie chleni: \n";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arrb[i];
-		copyb[i] = arrb[i];
-	}
-	double* X = gaussmethod(arra, arrb, n);
-	cout << "\nReshenie SLAU: " <<endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << X[i] << " ";
-	}
-	cout << endl;
-	double* F = vectornevyazki(copya, copyb, X, n);
-	cout << "\nVektor nevyazki F: " <<endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << F[i] << " ";
-	}
-	cout << endl;
-	cout << "Norma vectora nevyazki F: " <<endl;
-	double normaVektora = norma_vektora(F, n);
-	cout << normaVektora;
-	double* b = vectorb(copya, X, n);
-	double* X2 = gaussmethod(copya, b, n);
-	cout << "\nReshenie vspomagatelnoi sistemi : " <<endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << X2[i] << " ";
-	}
-	cout << endl;
-	cout << "Otnositelnaya pogreshnost: " << norma_vektora(raznost(X, X2, n), n) / norma_vektora(X, n) <<endl;
-	cout << endl;
-	system("pause");
-	return 0;
-}
 
 double* gaussmethod(double** arra, double* arrb, int n)
 {
@@ -164,3 +103,66 @@ double* raznost(double* X1, double* X2, int n)
 	}
 	return x;
 }
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	cout << "vvedite poryadok matrici: ";
+	do
+	{
+		cin >> n;
+	} while (n < 0);
+	double** arra = new double* [n];
+	double* arrb = new double[n];
+	double** copya = new double* [n];
+	double* copyb = new double[n];
+	cout << "\nVvedide matricu: \n";
+	for (int i = 0; i < n; i++)
+	{
+		arra[i] = new double[n];
+		copya[i] = new double[n];
+		for (int j = 0; j < n; j++)
+		{
+			cout << "a[" << i + 1 << "][" << j + 1 << "]: ";
+			cin >> arra[i][j];
+			copya[i][j] = arra[i][j];
+		}
+	}
+	cout << "\nvvedite svobodnie chleni: \n";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arrb[i];
+		copyb[i] = arrb[i];
+	}
+	double* X = gaussmethod(arra, arrb, n);
+	cout << "\nReshenie SLAU: " <<endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << X[i] << " ";
+	}
+	cout << endl;
+	double* F = vectornevyazki(copya, copyb, X, n);
+	cout << "\nVektor nevyazki F: " <<endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << F[i] << " ";
+	}
+	cout << endl;
+	cout << "Norma vectora nevyazki F: " <<endl;
+	double normaVektora = norma_vektora(F, n);
+	cout << normaVektora;
+	double* b = vectorb(copya, X, n);
+	double* X2 = gaussmethod(copya, b, n);
+	cout << "\nReshenie vspomagatelnoi sistemi : " <<endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << X2[i] << " ";
+	}
+	cout << endl;
+	cout << "Otnositelnaya pogreshnost: " << norma_vektora(raznost(X, X2, n), n) / norma_vektora(X, n) <<endl;
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
